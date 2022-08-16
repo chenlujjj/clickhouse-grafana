6d8aec2ddedb5b69570767064ae23b79176d6edb
chenlujjj <953546398@qq.com>
2022-08-16 19:50:57 +0800

    增加打包脚本

---
+++ package_plugin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export GRAFANA_PLUGIN_ID=$(cat dist/plugin.json | jq -r .id)
+export GRAFANA_PLUGIN_VERSION=$(cat dist/plugin.json | jq -r .info.version)
+export GRAFANA_PLUGIN_TYPE=$(cat dist/plugin.json | jq -r .type)
+export GRAFANA_PLUGIN_ARTIFACT=${GRAFANA_PLUGIN_ID}-${GRAFANA_PLUGIN_VERSION}.zip
+export GRAFANA_PLUGIN_ARTIFACT_CHECKSUM=${GRAFANA_PLUGIN_ARTIFACT}.md5
+
+mkdir -p /tmp/${GRAFANA_PLUGIN_ID}
+cp -rfv dist/* /tmp/${GRAFANA_PLUGIN_ID}/
+pushd /tmp/
+zip ${GRAFANA_PLUGIN_ARTIFACT} ${GRAFANA_PLUGIN_ID} -r
+md5sum ${GRAFANA_PLUGIN_ARTIFACT} > /tmp/${GRAFANA_PLUGIN_ARTIFACT_CHECKSUM}
+popd
