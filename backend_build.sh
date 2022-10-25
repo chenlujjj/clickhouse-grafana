3a13ada6d01eb27fd6b71a54f3fbfe01f99cf3b2
chenlujjj <953546398@qq.com>
2022-10-25 16:57:48 +0800

    change dist binary name

--- backend_build.sh
+++ backend_build.sh
@@ -3,9 +3,9 @@
 set -ex
 
 go test -timeout 1m -failfast -tags=integration -run "${RUN_TESTS:-.+}" -v ./pkg/ &&
-GOOS=linux GOARCH=amd64 go build -o ./dist/altinity-clickhouse-plugin_linux_amd64 ./pkg/ &&
-GOOS=linux GOARCH=arm64 go build -o ./dist/altinity-clickhouse-plugin_linux_arm64 ./pkg/ &&
-GOOS=windows GOARCH=amd64 go build -o ./dist/altinity-clickhouse-plugin_windows_amd64.exe ./pkg/ &&
-GOOS=darwin GOARCH=amd64 go build -o ./dist/altinity-clickhouse-plugin_darwin_amd64 ./pkg/ &&
-GOOS=darwin GOARCH=arm64 go build -o ./dist/altinity-clickhouse-plugin_darwin_arm64 ./pkg/ &&
-chmod +x ./dist/altinity-clickhouse-plugin*
+GOOS=linux GOARCH=amd64 go build -o ./dist/bilibili-altinity-clickhouse-plugin_linux_amd64 ./pkg/ &&
+GOOS=linux GOARCH=arm64 go build -o ./dist/bilibili-altinity-clickhouse-plugin_linux_arm64 ./pkg/ &&
+GOOS=windows GOARCH=amd64 go build -o ./dist/bilibili-altinity-clickhouse-plugin_windows_amd64.exe ./pkg/ &&
+GOOS=darwin GOARCH=amd64 go build -o ./dist/bilibili-altinity-clickhouse-plugin_darwin_amd64 ./pkg/ &&
+GOOS=darwin GOARCH=arm64 go build -o ./dist/bilibili-altinity-clickhouse-plugin_darwin_arm64 ./pkg/ &&
+chmod +x ./dist/bilibili-altinity-clickhouse-plugin*
